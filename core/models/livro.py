8c88d36cdecd504da7d7cbb665a66602ce46f7e6
= <=>
2024-08-12 14:25:09 -0300

    livro com autor e coutor

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+from .autor import Autor
 from .categoria import Categoria
 from .editora import Editora
+
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
@@ -10,6 +12,7 @@ class Livro(models.Model):
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros", blank=True, null=True)
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", blank=True, null=True)
+    autor = models.ManyToManyField(Autor, related_name="livros", blank=True)
 
     def __str__(self):
         return f"({self.id}) {self.titulo} ({self.quantidade})"
